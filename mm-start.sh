0da4550e4fb4f0c6be62d4abf4816d7fae824d15
778a0a <778a0a@778a0a.com>
2024-09-28 12:46:43 +0900

    起動スクリプト更新 (メモリ削減)

--- mm-start.sh
+++ mm-start.sh
@@ -1,4 +1,14 @@
 conda init
 conda activate microblog
 poetry run inv compile-scss
-VENV_DIR=/home/ubuntu/miniconda3/envs/microblog poetry run supervisord -c misc/supervisord.conf -n
+
+# フル機能版
+# VENV_DIR=/home/ubuntu/miniconda3/envs/microblog poetry run supervisord -c misc/supervisord.conf -n
+
+# メモリ節約のためincoming/outgoing workerは立ち上げない。
+while true; do
+    timestamp=$(date '+%Y%m%d_%H%M%S')
+    echo --- Start uvicorn at ${timestamp} ---
+    datetimestamp=$(date '+%Y%m%d')
+    uvicorn app.main:app --no-server-header 2>&1 | tee logs/uvicorn_${datetimestamp}.log
+done
