d5a2a42ed15a72fa3e1e5eebc546088f2e1283c7
778a0a <778a0a@778a0a.com>
2024-10-17 20:43:03 +0900

    gifモード削除

--- app/static/common.js
+++ app/static/common.js
@@ -1,37 +1,3 @@
-function hasAudio (video) {
-    return video.mozHasAudio ||
-    Boolean(video.webkitAudioDecodedByteCount) ||
-    Boolean(video.audioTracks && video.audioTracks.length);
-}
-
-function setVideoInGIFMode(video) {
-    if (!hasAudio(video)) {
-        if (typeof video.loop == 'boolean' && video.duration <= 10.0) {
-            video.classList.add("video-gif-mode");
-            video.loop = true;
-            video.controls = false;
-            video.addEventListener("mouseover", () => {
-                video.play();
-            })
-            video.addEventListener("mouseleave", () => {
-                video.pause();
-            })
-        }
-    };
-}
-
-var items = document.getElementsByTagName("video")
-for (var i = 0; i < items.length; i++) {
-    if (items[i].duration) {
-        setVideoInGIFMode(items[i]);
-    } else {
-        items[i].addEventListener("loadeddata", function() {
-            setVideoInGIFMode(this);
-        });
-    }
-}
-
-
 const fullscreenContainer = document.getElementById('fullscreenImgContainer');
 if (fullscreenContainer != null) {
     const images = document.querySelectorAll('.attachment.u-photo');
